56f442e5fb6fdebd9a9994ad2b5f5b0d61fcda89
lucas-emg <lucas.gabriel3.lg@gmail.com>
2022-04-15 12:12:54 -0300

    app.css was defined with global stylings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import NavLinkPlato from '../design.system/StyledNavLink'
 const Navbar = () => {
   return (
     <nav>
+    <h1>Plato</h1>
     <NavLinkPlato to="/">Home</NavLinkPlato>
     <NavLinkPlato logout to="/">Logou</NavLinkPlato>
     </nav>
