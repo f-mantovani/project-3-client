5b38baf6a4e920399a72ca31ecc244c8d0e166bc
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-20 05:09:27 +0200

    fixed color to add new task

--- src/pages/MyTasks/MyTasks.js
+++ src/pages/MyTasks/MyTasks.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Navbar from '../../components/Navbar'
 import NoTasks from '../../components/Tasks/NoTasks'
 import TaskGroup from '../../components/Tasks/TaskGroup'
-import AddTaskPlato from '../../design.system/AddTaskPlato'
+import CardPlato from '../../design.system/CardPlato'
 import LineBreakPlato from '../../design.system/LineBreakePlato'
 import TabPlato from '../../design.system/TabPlato'
 
@@ -10,24 +10,24 @@ const types = ['To Do', 'Doing', 'Done']
 
 // Some data mocking for rendering test
 
-// const kanban = [
-//   { title: 'fazer html', status: 'todo', _id: '1' },
-//   { title: 'fazer css', status: 'todo', _id: '2' },
-//   { title: 'fazer css', status: 'todo', _id: '3' },
-//   { title: 'fazer css', status: 'todo', _id: '4' },
-//   { title: 'fazer css', status: 'todo', _id: '5' },
-//   { title: 'fazer css', status: 'todo', _id: '6' },
-//   { title: 'fazer css', status: 'todo', _id: '7' },
-//   { title: 'fazer css', status: 'todo', _id: '8' },
-//   { title: 'doing1', status: 'doing', _id: '9' },
-//   { title: 'doing2', status: 'doing', _id: '10' },
-//   { title: 'doing3', status: 'doing', _id: '11' },
-//   { title: 'doing3', status: 'doing', _id: '12' },
-//   { title: 'done1', status: 'done', _id: '13' },
-//   { title: 'done2', status: 'done', _id: '14' },
-// ]
+const kanban = [
+  { title: 'fazer html', status: 'todo', _id: '1' },
+  { title: 'fazer css', status: 'todo', _id: '2' },
+  { title: 'fazer css', status: 'todo', _id: '3' },
+  { title: 'fazer css', status: 'todo', _id: '4' },
+  { title: 'fazer css', status: 'todo', _id: '5' },
+  { title: 'fazer css', status: 'todo', _id: '6' },
+  { title: 'fazer css', status: 'todo', _id: '7' },
+  { title: 'fazer css', status: 'todo', _id: '8' },
+  { title: 'doing1', status: 'doing', _id: '9' },
+  { title: 'doing2', status: 'doing', _id: '10' },
+  { title: 'doing3', status: 'doing', _id: '11' },
+  { title: 'doing3', status: 'doing', _id: '12' },
+  { title: 'done1', status: 'done', _id: '13' },
+  { title: 'done2', status: 'done', _id: '14' },
+]
 
-const kanban = []
+// const kanban = []
 
 const MyTasks = () => {
   const [active, setActive] = useState(types[0])
@@ -52,24 +52,25 @@ const MyTasks = () => {
         </div>
         <LineBreakPlato />
       </div>
-      {(!kanban.length || active === types[0]) && (
-        <AddTaskPlato>
-          <input
-            type='checkbox'
-            id='add-new-task'
-            className='custom-checkbox '
-          />
-          <label htmlFor='add-new-task' className='task-label body'>
-            Add new task..
-          </label>
-        </AddTaskPlato>
-      )}
-      {!kanban.length && <NoTasks />}
       {active === types[0] && (
         <TaskGroup kanban={kanban} name='todo'>
           To Do
         </TaskGroup>
       )}
+      {(!kanban.length || active === types[0]) && (
+        <CardPlato muted>
+          <input
+            type='checkbox'
+            id='add-new-task'
+            className='custom-checkbox'
+          />
+          <label htmlFor='add-new-task' className='task-label body'>
+            Add new task...
+          </label>
+        </CardPlato>
+      )}
+      {!kanban.length && <NoTasks />}
+
       {active === types[1] && (
         <TaskGroup kanban={kanban} name='doing'>
           Doing
