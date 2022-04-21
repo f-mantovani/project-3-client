ea65e661f07ebbe84f81b93372dd0b070b95abb5
lucas-emg <lucas.gabriel3.lg@gmail.com>
2022-04-21 20:14:03 -0300

    top menu is complete

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+import { useState } from 'react'
+import TabPlato from '../../design.system/TabPlato'
+import LineBreak from '../../design.system/LineBreakePlato'
 
 const Dashboard = () => {
+
+  const types = ['Overview', 'Progress']
+
+  const [active, setActive] = useState(types[0])
+
+
   return (
     <div>
       <div className="page-header">
         <Navbar/>
         <h1>Dashboard</h1>
       </div>
+      <div className='tab-header'>
+        <div className='tabs'>
+          {types.map((type) => (
+            <TabPlato
+              key={type}
+              className={active === type && 'active'}
+              onClick={() => setActive(type)}
+            >
+              <p className='label'>{type}</p>
+            </TabPlato>
+          ))}
+
+          
+        </div>
+        <LineBreak />
+        </div>
+
+
         
     </div>
   )
