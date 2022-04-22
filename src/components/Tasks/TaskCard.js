4f6190ed047501079ac859225937934640f9b760
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-22 04:01:41 +0200

    finally works right with the checkbox

--- src/components/Tasks/TaskCard.js
+++ src/components/Tasks/TaskCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import './TaskCard.css'
 import CardPlato from '../../design.system/CardPlato'
+import CustomCheckboxPlato from '../../design.system/CustomCheckboxPlato'
 import ColumnContainer from '../../design.system/ColumnContainer'
 import RequiredPlato from '../../design.system/RequiredPlato'
 import { Body } from '../../design.system/text.styling/styles'
@@ -8,7 +8,7 @@ import { Body } from '../../design.system/text.styling/styles'
 const TaskCard = ({ title, status, index }) => {
   return (
     <CardPlato className='task-card'>
-      <div className='checkbox-container'>
+      <CustomCheckboxPlato>
         <input type='checkbox' id={index} className='custom-checkbox' />
         <label
           htmlFor={index}
@@ -16,7 +16,7 @@ const TaskCard = ({ title, status, index }) => {
         >
           <Body done={status === 'done'}  muted={status === 'done'} >{title}</Body>
         </label>
-      </div>
+      </CustomCheckboxPlato>
       <ColumnContainer kebab='true'>
         <RequiredPlato />
         <RequiredPlato />
---
+++ src/design.system/CustomCheckboxPlato.js
@@ -0,0 +1,55 @@
+import styled, { css } from 'styled-components'
+import checkIcon from '../assets/check-circle3x.png'
+
+const icon = css`url("../assets/check-circle3x.png")`
+
+const CustomCheckboxPlato = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  .task-label {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  }
+
+  .task-label::before {
+  content: '';
+  width: 1.25em;
+  height: 1.25em;
+  border: 1px solid rgba(241, 241, 241, 0.5);
+  border-radius: 100%;
+  margin-right: 0.75em;
+  }
+
+  .custom-checkbox{
+  cursor: pointer;
+  position: absolute;
+  opacity: 0;  
+  }
+
+  .task-label:hover::before,
+  .custom-checkbox:hover + .task-label::before {
+    background-color: black;
+  }
+
+  .custom-checkbox:focus + .task-label::before {
+  border: .05em slateblue solid;
+  box-shadow: 2px 2px 2px slateblue;
+  }   
+
+  .custom-checkbox:checked + .task-label::before{
+    content: '';
+    border: none;
+    width: 1.25rem;
+    height: 1.25rem;
+    background-image: url(${checkIcon});
+    background-size: 1.25rem 1.25rem;
+    background-repeat: no-repeat;
+  }
+
+  
+`
+
+export default CustomCheckboxPlato
