17a14393c48cdc8c4451e7b54d5def891b8df850
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-27 00:35:51 +0200

    made the collapsable menu div

---
+++ src/components/MenuCollapsable.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MenuPlato from '../design.system/MenuPlato'
+
+const MenuCollapsable = ({ children }) => {
+  return (
+    <MenuPlato>{children}</MenuPlato>
+  )
+}
+
+export default MenuCollapsable
---
+++ src/design.system/MenuPosition.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+const MenuPosition = styled.div`
+  position:absolute;
+  top: -30%;
+  left:-525%;
+`
+
+export default MenuPosition
