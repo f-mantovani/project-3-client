b6c653da229a5844b4c8ce9f3077c19acb3f242e
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-28 01:27:12 +0200

    changed the navbar button to be positioned with flexbox

--- src/design.system/PageHeaderPlato.js
+++ src/design.system/PageHeaderPlato.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 
 const PageHeaderPlato = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   min-height: 3rem;
   padding-top: 1em;
   margin-bottom: .75rem;
