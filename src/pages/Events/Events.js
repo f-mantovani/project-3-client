4a1f4f4d22fe692ed31f4f8e1d75ea66f2358fce
lucas-emg <lucas.gabriel3.lg@gmail.com>
2022-04-26 21:00:02 -0300

    page set up is done

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import { useState } from 'react'
 import Navbar from '../../components/Navbar'
+import LineBreak from '../../design.system/LineBreakePlato'
+import PageHeaderPlato from '../../design.system/PageHeaderPlato'
+import TabHeaderPlato from '../../design.system/TabHeaderPlato'
+import TabPlato from '../../design.system/TabPlato'
 
 const Events = () => {
+
+  const types = ['Upcoming', 'Previous']
+
+  const [active, setActive] = useState(types[0])
+
+
   return (
     <div>
     <Navbar/>
-    Events
+    <PageHeaderPlato>
+        <Navbar/>
+        <h1>Events</h1>
+      </PageHeaderPlato>
+      <TabHeaderPlato>
+        <div className='tabs'>
+          {types.map((type) => (
+            <TabPlato
+              key={type}
+              active={active === type && 'active'}
+              onClick={() => setActive(type)}
+            >
+              <p className='label'>{type}</p>
+            </TabPlato>
+          ))}
+
+          
+        </div>
+        </TabHeaderPlato>
+
+        <LineBreak />  
+
+        {active === types[0] && 
+
+        <p>Upcoming</p>}
+
+        {active === types[1] && 
+
+        <p>Previous</p>}
     </div>
   )
 }
