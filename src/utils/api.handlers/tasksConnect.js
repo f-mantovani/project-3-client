d81e2c02f36dc17fa28e6f015d3992d1170a7afb
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-30 18:19:21 +0200

    method createTask added

--- src/utils/api.handlers/tasksConnect.js
+++ src/utils/api.handlers/tasksConnect.js
@@ -13,6 +13,15 @@ class tasksConnect extends apiConstructor {
       console.log(error.response.data)
     }
   }
+
+  createTask = async (newTask) => {
+    try {
+      const { data } = await this.api.post('/', newTask)
+      return data
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
 }
 
 export default new tasksConnect ()
