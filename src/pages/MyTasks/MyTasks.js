fa7f931afecbf5f7b1b71be9b26ed82c7fae4c83
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-30 21:06:56 +0200

    Create task connected to DB using custom hook

--- src/pages/MyTasks/MyTasks.js
+++ src/pages/MyTasks/MyTasks.js
@@ -18,27 +18,6 @@ import { useQuery } from "react-query";
 
 const types = ['To Do', 'Doing', 'Done']
 
-// Some data mocking for rendering test
-
-// const kanban = [
-//   { title: 'fazer html', status: 'todo', _id: '1' },
-//   { title: 'fazer css', status: 'todo', _id: '2' },
-//   { title: 'fazer css', status: 'todo', _id: '3' },
-//   { title: 'fazer css', status: 'todo', _id: '4' },
-//   { title: 'fazer css', status: 'done', _id: '5' },
-//   { title: 'fazer css', status: 'done', _id: '6' },
-//   { title: 'fazer css', status: 'done', _id: '7' },
-//   { title: 'fazer css', status: 'doing', _id: '8' },
-//   { title: 'doing1', status: 'doing', _id: '9' },
-//   { title: 'doing2', status: 'doing', _id: '10' },
-//   { title: 'doing3', status: 'doing', _id: '11' },
-//   { title: 'doing3', status: 'doing', _id: '12' },
-//   { title: 'done1', status: 'done', _id: '13' },
-//   { title: 'done2', status: 'done', _id: '14' },
-// ]
-
-// const kanban = []
-
 const MyTasks = () => {
   
   const [active, setActive] = useState(types[0])
@@ -49,7 +28,6 @@ const MyTasks = () => {
 
   if (error) return "An error has occurred: " + error.message;
  
-
   return (
     <div>
       <PageHeaderPlato>
--- src/utils/controllers/useTaskForm.js
+++ src/utils/controllers/useTaskForm.js
@@ -1,15 +1,17 @@
 import { useState } from 'react'
+import tasksConnect from '../api.handlers/tasksConnect'
+import useAsyncMutation from './useAsyncMutation'
 
 const useTaskForm = () => {
   const [ newTask, setNewTask ] = useState('')
+  const createTask = useAsyncMutation(tasksConnect.createTask, 'kanban')
 
   const handleTaskInput = (e) => {
     setNewTask(e.target.value)
   }
 
   const saveNewTask = () => {
-    // Here goes api call
-    console.log(newTask)
+    createTask({ title: newTask })
     setNewTask('')
   }
   return { newTask, handleTaskInput, saveNewTask }
