6bfe300c25305a2d2f8bd1c425b6fbefcf6727b5
lucas-emg <lucas.gabriel3.lg@gmail.com>
2022-05-05 22:00:59 -0300

    user info is being correctly pulled into page

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,9 +1,12 @@
 import {useState, useEffect } from 'react'
 import Navbar from '../../components/Navbar'
 import PageHeaderPlato from '../../design.system/PageHeaderPlato'
-import TabHeaderPlato from '../../design.system/TabHeaderPlato'
-import { Body } from '../../design.system/text.styling/styles'
 import userConnect from '../../utils/api.handlers/userConnect'
+import ColumnContainer from '../../design.system/ColumnContainer'
+import noUserPhoto from '../../assets/user-photo.png'
+import RowContainer from '../../design.system/RowContainer'
+import { Body } from '../../design.system/text.styling/styles'
+import editPencil from '../../assets/edit-pencil.png'
 
 const User = () => {
     const [user, setUser] = useState({})
@@ -16,13 +19,38 @@ const User = () => {
         getUser()
       }, [])
 
+      const { name, email, image } = user
+
   return (
     <div>
         <PageHeaderPlato>
         <h1>Profile</h1>
         <Navbar />
       </PageHeaderPlato>
-      <TabHeaderPlato></TabHeaderPlato>
+
+      <ColumnContainer>
+          <div>
+            {image
+            ? <img src={image} alt="User Profile"/>
+            : <img src={noUserPhoto} alt="User Profile"/>
+            }
+          </div>
+
+          <img src={editPencil} alt="Edit user info"/>
+
+          <RowContainer>
+              <Body>Name:</Body>
+              <Body>{name}</Body>
+
+          </RowContainer>
+          <RowContainer>
+              <Body>Email:</Body>
+              <Body>{email}</Body>
+          </RowContainer>
+      </ColumnContainer>
+
+
+
     </div>
   )
 }
