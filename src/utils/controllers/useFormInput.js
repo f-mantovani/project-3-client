94eae8b3b4197cf9c5add4e94ff12fc21fbfc07a
Felipe Mantovani <92172133+F-Mantovani@users.noreply.github.com>
2022-05-14 02:22:04 +0200

    initial commit

--- src/utils/controllers/useFormInput.js
+++ src/utils/controllers/useFormInput.js
@@ -1,48 +1,58 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import apiConnect from '../api.handlers/apiConnect'
-import saveToken from './saveToken'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import apiConnect from "../api.handlers/apiConnect";
+import saveToken from "./saveToken";
 
 const useFormInput = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const navigate = useNavigate()
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
-  const handleNameInput = (e) => { 
-    setName(e.target.value)
-  }
-  const handleEmailInput = (e) => { 
-    setEmail(e.target.value)
-  }
+  const handleNameInput = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmailInput = (e) => {
+    setEmail(e.target.value);
+  };
   const handlePasswordInput = (e) => {
-    setPassword(e.target.value)
-   }
+    setPassword(e.target.value);
+  };
 
   const loginUser = async () => {
-    const logUser = {email, password}
+    const logUser = { email, password };
     try {
-      const data = await apiConnect.logIn(logUser)
-      saveToken(data)
-      navigate('/')
-      setEmail('')
-      setPassword('')
+      const data = await apiConnect.logIn(logUser);
+      saveToken(data);
+      navigate("/");
+      setEmail("");
+      setPassword("");
     } catch (error) {
-      throw error
+      throw error;
     }
-  }
+  };
   const signUpUser = async () => {
-    const newUser = {name, email, password}
+    const newUser = { name, email, password };
     try {
-      await apiConnect.signUp(newUser)
-      await loginUser()
-      setName('') 
+      await apiConnect.signUp(newUser);
+      await loginUser();
+      setName("");
     } catch (error) {
-      throw error
+      throw error;
     }
-  }
+  };
 
-  return { name, email, password, handleEmailInput, handleNameInput, handlePasswordInput, loginUser, signUpUser }
-}
+  return {
+    name,
+    email,
+    password,
+    handleEmailInput,
+    handleNameInput,
+    handlePasswordInput,
+    loginUser,
+    signUpUser
+  };
+};
 
-export default useFormInput
+export default useFormInput;
