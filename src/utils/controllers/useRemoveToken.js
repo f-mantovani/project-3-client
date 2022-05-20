e7a647a8ca350cc9a27e4fae4bfac5c7842238f6
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-05-20 11:56:28 +0200

    fixing some problems with removingToken

--- src/utils/controllers/useRemoveToken.js
+++ src/utils/controllers/useRemoveToken.js
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom"
+
 
 const useRemoveToken = (error) => {
+
+  const navigate = useNavigate()
+
   if (localStorage.getItem('token') || error.response.status === 401){ 
     localStorage.removeItem('token')
+    navigate('/')
   }
 }
 
