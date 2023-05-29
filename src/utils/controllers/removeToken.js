96ee081664e8702fcbacc1adf3a423a3547612ed
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-05-29 10:42:47 +0200

    remove console.log

--- src/utils/controllers/removeToken.js
+++ src/utils/controllers/removeToken.js
@@ -1,15 +1,9 @@
-
-const removeToken = (error) => {
-
-  const token = localStorage.getItem('token')
-  console.log(error.response)
-  if (token && (error.response.status === 401 || error.response.data === 'jwt malformed')){ 
- 
-    localStorage.removeItem('token');
-    window.location.pathname = '/'
-  }   
-
-
+const removeToken = error => {
+	const token = localStorage.getItem('token')
+	if (token && (error.response.status === 401 || error.response.data === 'jwt malformed')) {
+		localStorage.removeItem('token')
+		window.location.pathname = '/'
+	}
 }
 
 export default removeToken
